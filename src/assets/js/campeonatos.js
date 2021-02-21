0f1d69438f1e17767e16de6357298e55e474c10a
jdromero88 <jdromerov88@gmail.com>
2021-02-21 16:11:53 -0500

    Trabajando en como mostrar los datos usando highcharts

--- src/assets/js/campeonatos.js
+++ src/assets/js/campeonatos.js
@@ -1,5 +1,8 @@
 /*Use Papa parse and Datatables to read Google Spreadsheet*/
-
+let data = []
+let nombres = []
+let nombreCampeones = []
+let losCampeones = []
 let publicSpreadsheetUrl =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vSRaO91LVh7cnt2dUYI3qFgheM-rPVG5ARaLbmRFH9q_HdoyLIMsYLlr-6Htka4IvocfPnNnBgQ6PhT/pub?output=csv";
 
@@ -11,17 +14,76 @@ function init() {
       header: true,
       complete: showInfo
     })
+
+
+    // highcharts
+    // eslint-disable-next-line
+    Highcharts.chart('container', {
+        chart: {
+            type: 'column',
+            styledMode: true
+        },
+
+        title: {
+            text: 'Styling axes and columns'
+        },
+
+        yAxis: [{
+            className: 'highcharts-color-0',
+            title: {
+                text: 'Primary axis'
+            }
+        }, {
+            className: 'highcharts-color-1',
+            opposite: true,
+            title: {
+                text: 'Secondary axis'
+            }
+        }],
+
+        plotOptions: {
+            column: {
+                borderRadius: 5
+            }
+        },
+
+        series: [{
+            data: showClubs(data)
+
+        }, {
+            data: [324, 124, 547, 221],
+            yAxis: 1
+        }]
+
+    });
+    // highcharts
 }
 
 function showInfo(results) {
-  let data = results.data
-  displayInfo(data);
+  data = results.data
+  nombreCampeones = showClubs(data)
+  losCampeones = campeonatosPorClub(data, showClubs(data))
+  console.log("Nombres:", nombreCampeones)
+  console.log("Lista campeones: ",losCampeones)
 }
 
-function displayInfo(data) {
-  console.log(data);
-  let olimpia = data.filter( club => club.Club === "Olimpia")
-  console.log(olimpia);
+function showClubs(data) {
+  let clubs = []
+  data.forEach( item => {
+    if ( item.Club !== "" && item.Club !== "No hubo campeonato" ) {
+      clubs.push( item.Club )
+    }
+  });
+  nombres = [...new Set(clubs)]
+  return nombres
 }
 
+function campeonatosPorClub(data, nombres) {
+  let totalCampeonatos = []
+  nombres.forEach( name => {
+    let porClub = data.filter( nombre => nombre.Club === name)
+    totalCampeonatos.push(porClub)
+  })
+  return totalCampeonatos
+}
 window.addEventListener("DOMContentLoaded", init);
